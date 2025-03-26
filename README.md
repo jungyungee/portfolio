2cc6af5f0a178daa73f708a8476354c89b6abd00
jungyungee <jungyungee0610@gmail.com>
2025-03-27 03:07:42 +0900

    [feat]헤더 추가, 페이지 내 section 분리 및 제목 typewrite 효과

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Portfolio web page (포트폴리오용 웹사이트)
-=======
+
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
 ## Getting Started
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,39 @@
+'use client';
 import Image from "next/image";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="font-[family-name:var(--font-geist-sans)] text-white">
+      <header className="fixed top-0 left-0 w-full bg-[#0a1e3f] text-white z-50 shadow-md font-bold font-mono">
+        <nav className="flex justify-center gap-8 p-4 text-lg font-medium">
+          <a href="#about" className="hover:underline"> {"<about />"}</a>
+          <a href="#skills" className="hover:underline"> {"<skills />"}</a>
+          <a href="#projects" className="hover:underline"> {"<projects />"}</a>
+        </nav>
+      </header>
+      <div className="scroll-smooth">
+        <section id="about" className="min-h-screen bg-[#1b1956] flex flex-col items-center justify-center p-4">
+          <h1 className="text-6xl font-bold">
+            <Typewriter
+              words={['정윤지', 'Yungee Jung']}
+              loop={false}
+              cursor
+              cursorStyle="_"
+              typeSpeed={150}
+              deleteSpeed={50}
+              delaySpeed={1500}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
+          </h1>
+          <p className="text-xl mt-4">프론트엔드 개발자</p>
+        </section>
+        <section id="skills" className="min-h-screen bg-[#1b1b1b] flex items-center justify-center p-10">
+          <p className="text-2xl">소개글이나 기술 스택</p>
+        </section>
+        <section id="projects" className="min-h-screen bg-[#0f0f4f] flex items-center justify-center p-10">
+          <p className="text-2xl">프로젝트</p>
+        </section>
+      </div>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
